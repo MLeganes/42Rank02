e9f13ce2484111e2528283191ecb0069cce6b96f
Angel Morcillo leganes <amorcill@1-B-8.42heilbronn.de>
2021-10-07 14:42:29 +0200

    ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -1,98 +1,119 @@
-#include <fcntl.h>
 #include <unistd.h>
 #include <stdarg.h>
 
-int printf_s(char *s)
-{
-	int i;
-	int ret;
+// Lib for testing
+#include <stdio.h>
 
+int	printf_s(char *s)
+{
+	int	i;
+	int	ret;
+
+	i = 0;
 	ret = 0;
 	while (s[i] != '\0')
-	{
-		ret += write(1, &s[i], 1);
-		i++;
-	}
+		ret += write(1, &s[i++], 1);
 	return (ret);
 }
 
-int ft_putptr(unsigned long hex, int reset)
+int	ft_putptr(unsigned long hex, int reset)
 {
-	int ret;
-	char c;
+	static int	ret;
+	char		c;
 
-	if( reset == 0 )
-	{
+	if (reset == 0)
 		ret = 0;
-	}
-	if(hex / 16)
-		ft_putptr(hex/16, 1);
-
-	if( (hex % 16) > 9)
+	if (hex / 16)
+		ft_putptr(hex / 16, 1);
+	if ((hex % 16) > 9)
 	{
 		c = 'a' + (hex % 16) - 10;
-	}else{
+	}
+	else
+	{
 		c = '0' + hex % 16;
 	}
 	ret += write(1, &c, 1);
 	return (ret);
 }
 
-int printf_p(unsigned long p)
+int	printf_p(unsigned long p)
 {
-	 int ret;
-	 ret = 0;
-	 if( p == 0 )
-	 {
-		 ret = write(1, "0x0", 3);
-	 }else
-	 {
+	int		ret;
+
+	ret = 0;
+	if (p == 0)
+	{
+		ret = write(1, "0x0", 3);
+	}
+	else
+	{
 		ret += write(1, "0x", 2);
-	 	ret += ft_putptr(p, 0);
-	 }
-	 return (ret);
+		ret += ft_putptr(p, 0);
+	}
+	return (ret);
 }
 
-int ft_puthex(unsigned int hex, int reset)
+int	ft_puthex(unsigned int hex, int reset)
 {
-	int ret;
-	char c;
+	static int	ret;
+	char		c;
 
-	if( reset == 0 )
+	if (reset == 0)
 	{
 		ret = 0;
 	}
-	if(hex / 16)
-		ft_puthex(hex/16, 1);
-
-	if( (hex % 16) > 9)
+	if (hex / 16)
+		ft_puthex(hex / 16, 1);
+	if ((hex % 16) > 9)
 	{
 		c = 'a' + (hex % 16) - 10;
-	}else{
+	}
+	else
+	{
 		c = '0' + hex % 16;
 	}
 	ret += write(1, &c, 1);
 	return (ret);
 }
 
-int printf_x(unsigned long x)
+int	printf_x(unsigned int x)
 {
 	return (ft_puthex(x, 0));
 }
 
-int ft_printf(const char *fmt, ...) /* '...' is C syntax for a variadic function */
+int	ft_putnbr(int d, int reset)
 {
-	va_list ap;
-	//char c;
-	char *s;
-	//int d;
-	int i;
-	int ret;
-	unsigned int x;
-	unsigned long p;
+	static int	ret;
+	char		nbr;
+
+	if (reset == 0)
+		ret = 0;
+	if (d / 10)
+		ft_putnbr(d / 10, 1);
+	nbr = '0' + (d % 10);
+	ret += write(1, &nbr, 1);
+	return (ret);
+}
+
+int	printf_d(int d)
+{
+	return (ft_putnbr(d, 0));
+}
+
+int	ft_printf(const char *fmt, ...) /* '...' is C syntax for a variadic function */
+{
+	va_list			ap;
+	char			*s;
+	int				i;
+	int				ret;
+	int				d;
+	unsigned int	x;
+	unsigned long	p;
 
 	ret = 0;
 	i = 0;
+	x = 0;
 	va_start(ap, fmt);
 	while (fmt[i])
 	{
@@ -108,14 +129,20 @@ int ft_printf(const char *fmt, ...) /* '...' is C syntax for a variadic function
 				s = va_arg(ap, char *);
 				ret += printf_s(s);
 			}
-			if (fmt[i] == 'p'){
-				p = (unsigned long) va_arg(ap, void *); // Pointer Unsigned long!!!!!!!!
+			if (fmt[i] == 'p')
+			{
+				p = (unsigned long)va_arg(ap, void *);
 				ret += printf_p(p);
 			}
 			if (fmt[i] == 'x')
 			{
-				x = va_arg(ap, unsigned int);// Hex Unsigned int!!!!!!!
+				x = va_arg(ap, unsigned int);
 				ret += printf_x(x);
+			}
+			if (fmt[i] == 'd')
+			{
+				d = va_arg(ap, int);
+				ret += printf_d(d);
 			}
 			//more types...
 		}
@@ -125,20 +152,19 @@ int ft_printf(const char *fmt, ...) /* '...' is C syntax for a variadic function
 	return (ret);
 }
 
-#include <stdio.h>
-
-int main()
+int	main(void)
 {
-	char *str = "Hola cara cola";
-	unsigned char hex;
+	char	*str = "Hello 42Heilbronn";
+	int		x = 0xAABB;
+	int		d = 4242;
 
-	hex = 0xA;
-
-	ft_printf("%s \n", str);
-	ft_printf("%p \n", &str);
-	ft_printf("%x \n", &hex);
-	printf("%s \n", str);
-	printf("%p \n", &str);
-	printf("%x \n", hex);
+	ft_printf(" Ret: %d \n", ft_printf("%s", str));
+	ft_printf(" Ret: %d \n", ft_printf("%p", &str));
+	ft_printf(" Ret: %d \n", ft_printf("%x", x));
+	ft_printf(" Ret: %d \n", ft_printf("%d", d));
+	printf(" Ret: %d \n", printf("%s", str));
+	printf(" Ret: %d \n", printf("%p", &str));
+	printf(" Ret: %d \n", printf("%x", x));
+	printf(" Ret: %d \n", printf("%d", d));
 	return (0);
 }
